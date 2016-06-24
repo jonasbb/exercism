9fd39f4a59e36fbe68fd0d0fd0a84ddc978ed822
Jonas Bushart <jonas@bushart.org>
2016-06-24 23:07:35 +0200

    ADD Rust: Custom Set

---
+++ rust/custom-set/src/lib.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+pub struct CustomSet<T>(Vec<T>);
+
+impl<T: PartialEq + Clone> CustomSet<T> {
+    pub fn new(data: Vec<T>) -> Self {
+        CustomSet(data)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn add(&mut self, value: T) {
+        self.0.push(value)
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        self.0.contains(value)
+    }
+
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.0.iter().all(|x| other.contains(x))
+    }
+
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        self.0.iter().all(|x| !other.contains(x))
+    }
+
+    pub fn difference(&self, other: &Self) -> Self {
+        CustomSet::new(self.0.iter().filter(|x| !other.contains(x)).map(|x| x.clone()).collect())
+    }
+
+    pub fn intersection(&self, other: &Self) -> Self {
+        CustomSet::new(self.0.iter().filter(|x| other.contains(x)).map(|x| x.clone()).collect())
+    }
+
+    pub fn union(&self, other: &Self) -> Self {
+        let mut tmp = self.0.clone();
+        tmp.extend(other.0.clone());
+        CustomSet::new(tmp)
+    }
+}
+
+impl<T: PartialEq + Clone> PartialEq for CustomSet<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.is_subset(other) && other.is_subset(self)
+    }
+}
