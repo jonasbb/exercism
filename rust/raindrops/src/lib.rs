819bec7ae2f306527656fdfaa2465a29604ee504
Jonas Bushart <jonas@bushart.org>
2016-06-24 18:33:19 +0200

    ADD Rust: Raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn raindrops(num: u64) -> String {
+    let mut res = String::with_capacity(15);
+    if num % 3 == 0 {
+        res.push_str("Pling")
+    };
+    if num % 5 == 0 {
+        res.push_str("Plang")
+    };
+    if num % 7 == 0 {
+        res.push_str("Plong")
+    };
+
+    // not divisible by anything above
+    if res.len() == 0 {
+        res.push_str(&num.to_string())
+    };
+    res
+}
