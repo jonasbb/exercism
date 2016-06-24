d64edf9018607dd41c4eb44512ccfbb99a91e92f
Jonas Bushart <jonas@bushart.org>
2016-06-24 18:43:51 +0200

    ADD Rust: Sieve

---
+++ rust/sieve/src/lib.rs
@@ -0,0 +1,22 @@
+use std::iter::FromIterator;
+
+pub fn primes_up_to(limit: usize) -> Vec<usize> {
+    // 0 represents non prime numbers
+    // non 0 values are prime
+    let mut res = Vec::from_iter(0..limit + 1);
+    res[0] = 0;
+    res[1] = 0;
+    // test up to sqrt(limit)
+    for x in 2..((limit as f64).sqrt().ceil() as usize) {
+        // already proven to be non-prime?
+        if res[x] == 0 {
+            continue;
+        } else {
+            // mark all multiple as non-prime
+            for v in 2..(limit / x) + 1 {
+                res[x * v] = 0;
+            }
+        }
+    }
+    res.into_iter().filter(|&x| x > 0).collect()
+}
