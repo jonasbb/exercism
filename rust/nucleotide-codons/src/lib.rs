e3f439d093506f871cc3195d3db2994e965e0320
Jonas Bushart <jonas@bushart.org>
2016-06-25 00:38:53 +0200

    ADD Rust: Nucleotide Codons

---
+++ rust/nucleotide-codons/src/lib.rs
@@ -0,0 +1,47 @@
+pub struct Mapping(Vec<(&'static str, &'static str)>);
+
+pub fn parse(data: Vec<(&'static str, &'static str)>) -> Mapping {
+    Mapping(data)
+}
+
+impl Mapping {
+    pub fn name_for(&self, short: &str) -> Result<&'static str, ()> {
+        for &(x, y) in self.0.iter() {
+            // assert that both iterators in the zip have the same length
+            if short.chars().count() != x.chars().count() {
+                continue;
+            }
+
+            // compare character for character
+            if short.chars().zip(x.chars()).all(|(a, b)| {
+                match a {
+                    // matches identity
+                    'A' | 'C' | 'G' | 'T' => a == b,
+
+                    'W' => b == 'A' || b == 'T',
+                    'S' => b == 'C' || b == 'G',
+                    'M' => b == 'A' || b == 'C',
+                    'K' => b == 'G' || b == 'T',
+                    'R' => b == 'A' || b == 'G',
+                    'Y' => b == 'C' || b == 'T',
+
+                    // matches everything except one
+                    'B' => b != 'A',
+                    'D' => b != 'C',
+                    'H' => b != 'G',
+                    'V' => b != 'T',
+
+                    // matches all
+                    'N' => true,
+
+                    // unknown char matches nothing
+                    _ => false,
+                }
+            }) {
+                return Ok(y);
+            }
+        }
+        // no match found
+        Err(())
+    }
+}
